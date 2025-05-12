9c5ef9e95c2586b7703748370a292b8f02519220
dmamanipar <dmamanipar@unsa.edu.pe>
2025-05-12 12:35:18 -0500

    cambios

--- ms-auth/src/main/java/com/example/msauth/service/impl/AuthUserServiceImpl.java
+++ ms-auth/src/main/java/com/example/msauth/service/impl/AuthUserServiceImpl.java
@@ -22,7 +22,7 @@ public class AuthUserServiceImpl implements AuthUserService {
 
     @Override
     public AuthUser save(AuthUserDto authUserDto) {
-        Optional<AuthUser> user = authUserRepository.findByUsername(authUserDto.getUserName());
+        Optional<AuthUser> user = authUserRepository.findByUserName(authUserDto.getUserName());
         if (user.isPresent())
             return null;
         String password = passwordEncoder.encode(authUserDto.getPassword());
@@ -42,7 +42,7 @@ public class AuthUserServiceImpl implements AuthUserService {
 
     @Override
     public TokenDto login(AuthUserDto authUserDto) {
-        Optional<AuthUser> user = authUserRepository.findByUsername(authUserDto.getUserName());
+        Optional<AuthUser> user = authUserRepository.findByUserName(authUserDto.getUserName());
         if (!user.isPresent())
             return null;
         if (passwordEncoder.matches(authUserDto.getPassword(), user.get().getPassword()))
@@ -58,7 +58,7 @@ public class AuthUserServiceImpl implements AuthUserService {
         if (!jwtProvider.validate(token))
             return null;
         String username = jwtProvider.getUserNameFromToken(token);
-        if (!authUserRepository.findByUsername(username).isPresent())
+        if (!authUserRepository.findByUserName(username).isPresent())
             return null;
         return new TokenDto(token);
     }
